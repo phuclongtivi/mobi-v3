ecfb2794a6623047157abc1b0f264dd4a5300990
unknown <phuclongtivi@gmail.com>
2026-09-06 20:28:24 +0700

    Long MOBI V3 FIXFLOW3-R3A remove Home music

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+const config: CapacitorConfig = {
+  appId: "live.phuclong.long",
+  appName: "Long",
+  webDir: "out",
+  server: { androidScheme: "https" }
+};
+export default config;
